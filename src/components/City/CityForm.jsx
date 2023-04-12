37fd19e19cc3657beb8abaf6174c2bd81aa407a1
AndreiMorariu <andreimorariu4140@gmail.com>
2023-04-12 16:46:19 +0300

    Added search suggestions

--- src/components/City/CityForm.jsx
+++ src/components/City/CityForm.jsx
@@ -1,30 +1,61 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { RxMagnifyingGlass } from 'react-icons/rx';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 import './CityForm.css';
+import cities from 'cities.json';
 
 function CityForm({ onSubmit }) {
-  const [city, setCity] = useState('');
+  const [input, setInput] = useState('');
+  const [options, setOptions] = useState();
+
+  const handleSelectOptionClick = (option) => {
+    setInput(option);
+  };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
-    onSubmit(city);
-    setCity('');
+    onSubmit(input);
+    setInput('');
   };
 
+  useEffect(() => {
+    const options = cities.filter((city) => city.name.startsWith(input));
+
+    const unique = [
+      ...new Map(options.map((item) => [item.name, item])).values(),
+    ];
+    setOptions(unique);
+  }, [input]);
+
+  const renderOptions =
+    input &&
+    options.slice(0, 4).map((option) => (
+      <li
+        className="option"
+        onClick={() => handleSelectOptionClick(option.name)}
+      >
+        <AiOutlineArrowRight />
+        {option.name}
+      </li>
+    ));
+
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-group">
-        <input
-          type="text"
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="Search for places..."
-          value={city}
-        />
-        <button type="submit">
-          <RxMagnifyingGlass size={20} />
-        </button>
-      </div>
-    </form>
+    <>
+      <form onSubmit={formSubmitHandler}>
+        <div className="form-group">
+          <input
+            type="text"
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Search for places..."
+            value={input}
+          />
+          <button type="submit">
+            <RxMagnifyingGlass size={20} />
+          </button>
+        </div>
+      </form>
+      <ul className="options">{renderOptions}</ul>
+    </>
   );
 }
 
